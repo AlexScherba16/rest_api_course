1ad890b98991ff1f3de9514baf9abe42ed5647a8
Alex Scherba <alexscherba16@gmail.com>
2022-05-04 14:18:57 +0300

    using logrus as a application logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"rest_api_course/config"
 	"rest_api_course/internal/comment"
@@ -9,23 +9,33 @@ import (
 	transportHTTP "rest_api_course/internal/transport/http"
 )
 
-// App - the struct which contains things like
-// pointers to database connections
-type App struct{}
+// App - contains application info
+type App struct {
+	Name    string
+	Version string
+}
 
 // Run - handles the startup of our application
 func (app *App) Run() error {
 
 	appConfig := config.NewAppConfig()
+	//log.SetFormatter(&log.JSONFormatter{})
+	log.WithFields(
+		log.Fields{
+			"AppName":    app.Name,
+			"AppVersion": app.Version,
+		}).Info("Setting up application")
 
 	var err error
 	db, err := database.NewDatabase(&appConfig.DBConfig)
 	if err != nil {
+		log.Error(err)
 		return err
 	}
 
 	err = database.MigrateDB(db)
 	if err != nil {
+		log.Error(err)
 		return err
 	}
 
@@ -35,7 +45,7 @@ func (app *App) Run() error {
 	handler.SetupRoutes()
 
 	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
-		fmt.Println("Failed to set up server")
+		log.Error("Failed to set up server")
 		return err
 	}
 
@@ -43,9 +53,11 @@ func (app *App) Run() error {
 }
 
 func main() {
-	app := App{}
+	app := App{
+		Name:    "Comments REST service",
+		Version: "1.0.0",
+	}
 	if err := app.Run(); err != nil {
-		fmt.Println("Error Starting Up")
-		fmt.Println(err)
+		log.Error("Error Starting Up", err)
 	}
 }
